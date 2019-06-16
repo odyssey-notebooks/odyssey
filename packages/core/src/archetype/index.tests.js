3f8eeec0c048bff1918bcd62ea011f51511cff68
Patrick Keenan <sirberus.io@gmail.com>
2019-06-16 16:10:55 -0700

    test: improved coverage of archetype/index.js interface

--- packages/core/src/archetype/index.tests.js
+++ packages/core/src/archetype/index.tests.js
@@ -1,8 +1,12 @@
-import { Archetype, newArchetype } from './index.js'
+import defaultExport, { Archetype, newArchetype } from './index.js'
 
 describe('exports', () => {
   it('has all expected exports defined', () => {
+    expect(defaultExport).toBeDefined()
     expect(Archetype).toBeDefined()
     expect(newArchetype).toBeDefined()
+  })
+  it('exports newArchetype as its default export', () => {
+    expect(defaultExport).toBe(newArchetype)
   })
 })
