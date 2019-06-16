b2e6d0996ffcbdf9fe4166b8bd770cd26c7858dd
Patrick Keenan <sirberus.io@gmail.com>
2019-06-16 16:06:36 -0700

    fix: bad export

--- packages/core/src/archetype/factory.js
+++ packages/core/src/archetype/factory.js
@@ -1,5 +1,5 @@
 import Archetype from './class.js'
 
-export function newArchetype(name, options = {}) {
+export default function newArchetype(name, options = {}) {
   return new Archetype({ name, ...options })
 }
