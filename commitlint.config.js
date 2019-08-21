52d09a0776e98edbd60bc9cf8969d85e0e42778b
Patrick Keenan <sirberus.io@gmail.com>
2019-08-21 11:51:56 -0700

    config(repo): customized commitlint types and scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'always', 'sentence-case'],
+    "scope-empty": [2, 'never'],
+    'scope-enum': [
+      2, 
+      'always', 
+      [
+        'repo',
+        'docs', 
+        'core', 
+        'components', 
+        'css', 
+        'app'
+      ]
+    ],
+    'type-enum': [
+      2, 
+      'always', 
+      [
+        'build',
+        'ci',
+        'chore',
+        'feat',
+        'fix',
+        'refactor',
+        'style',
+        'test',
+        'config'
+      ]
+    ]
+  }
 }
