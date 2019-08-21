cb79d872a912002b146970ae6ef8d9aedaac318b
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2019-08-21 15:31:10 -0700

    config(repo): use commitlint-config-monorepo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,33 +1,16 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['monorepo'],
   rules: {
-    'subject-case': [0, 'always', 'sentence-case'],
-    "scope-empty": [2, 'never'],
     'scope-enum': [
       2, 
       'always', 
       [
         'repo',
-        'docs', 
-        'core', 
-        'components', 
-        'css', 
+        'docs',
+        'core',
+        'components',
+        'css',
         'app'
-      ]
-    ],
-    'type-enum': [
-      2, 
-      'always', 
-      [
-        'build',
-        'ci',
-        'chore',
-        'feat',
-        'fix',
-        'refactor',
-        'style',
-        'test',
-        'config'
       ]
     ]
   }
