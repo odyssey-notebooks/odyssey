0971a5fb5492e2fdffa6adad36560e6b6d696cae
Patrick Keenan <sirberus.io@gmail.com>
2019-08-21 11:03:51 -0700

    chore: cleanup unused components and code

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,16 +1,12 @@
-import Checkbox from "./src/components/Checkbox.vue";
-import RichTextEditor from "./src/components/RichTextEditor.vue";
 import TitleField from "./src/fields/TitleField.vue";
 import MarkdownField from "./src/fields/MarkdownField.vue";
 
 const components = {
-  Checkbox,
-  RichTextEditor,
   TitleField,
   MarkdownField
 };
 
-export { components, Checkbox, RichTextEditor, TitleField, MarkdownField };
+export { components, TitleField, MarkdownField };
 
 const plugin = {
   install(Vue) {
