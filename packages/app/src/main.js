ea6308db7c9d5b9458b1cd410c7037e54a5ed217
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-02-02 19:57:19 -0800

    feat(app): remove quill; add tribute

--- packages/app/src/main.js
+++ packages/app/src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import "./plugins/quill";
+import "tributejs/dist/tribute.css";
+
 import "./plugins/kadabra";
 
 Vue.config.productionTip = false;
