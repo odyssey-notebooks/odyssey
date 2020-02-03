79a9725aa3936c69ba6195db0cf8bbea4591de72
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-02-02 20:07:18 -0800

    feat(app): add vuex

--- packages/app/src/main.js
+++ packages/app/src/main.js
@@ -4,9 +4,11 @@ import App from "./App.vue";
 import "tributejs/dist/tribute.css";
 
 import "./plugins/kadabra";
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount("#app");
