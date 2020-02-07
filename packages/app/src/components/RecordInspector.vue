44a5115fe86c159f779c7ee523fbfd0f7c1e2eca
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-02-06 20:39:45 -0800

    feat(app): scope styles in RecordInspector

--- packages/app/src/components/RecordInspector.vue
+++ packages/app/src/components/RecordInspector.vue
@@ -42,7 +42,7 @@ export default {
   components: {
     TitleField,
     InlineTextField,
-    MarkdownField,
+    MarkdownField
   },
   computed: {
     selectedRecord() {
@@ -119,7 +119,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .record-inspector {
   width: 100%;
   height: 100%;
