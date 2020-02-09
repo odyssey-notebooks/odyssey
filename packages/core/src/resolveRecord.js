524665c5b3492d3877a7ed84cdf3461902baeafd
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-02-09 02:34:44 -0800

    fix(components): dedupe resolveRecord meta; pass full archetype

--- packages/core/src/resolveRecord.js
+++ packages/core/src/resolveRecord.js
@@ -1,14 +1,15 @@
 export default function resolveRecord(record, archetype) {
   const resolvedRecord = {
     _id: record._id,
-    __meta__: record.__meta__
+    __meta__: { ...record.__meta__ }
   }
+  resolvedRecord.__meta__.archetype = archetype
   for (const field of archetype.fields) {
     resolvedRecord[field.key] = {
       ...field,
       value: record[field.key] === undefined
-        ? field.default
-        : record[field.key]
+      ? field.default
+      : record[field.key]
     }
   }
   return resolvedRecord
