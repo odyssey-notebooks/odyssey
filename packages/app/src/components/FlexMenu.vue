ec91a0becb6676a20d0e355cc6f78e5cd131cdb3
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-02-15 19:25:42 -0800

    feat(app): sort core types into tabs

--- packages/app/src/components/FlexMenu.vue
+++ packages/app/src/components/FlexMenu.vue
@@ -1,20 +1,63 @@
 <template>
   <aside class="flex-menu">
       
-    <template v-if="$store.state.activeMenuTab === 'explorer'">
+    <template v-if="activeMenuTab === 'explorer'">
       <record-browser
-        v-for="archetype in $store.getters.archetypes.sort((a, b) => a.name > b.name ? 1 : -1)"
+        v-for="archetype in sortedArchetypes"
         :archetype="archetype"
         :key="archetype.name"
         collapsed
       />
     </template>
+
+    <template v-else-if="activeMenuTab === 'notes'">
+      <record-browser
+        :archetype="archetypeNamed('Daily Journal Entry')"
+        collapsed
+      />
+      <record-browser
+        :archetype="archetypeNamed('Note')"
+      />
+    </template>
+
+    <template v-else-if="activeMenuTab === 'places'">
+      <record-browser
+        :archetype="archetypeNamed('Location')"
+      />
+    </template>
+
+    <template v-else-if="activeMenuTab === 'people'">
+      <record-browser
+        :archetype="archetypeNamed('Person')"
+      />
+    </template>
+
+    <template v-else-if="activeMenuTab === 'tasks'">
+      <record-browser
+        :archetype="archetypeNamed('Repeating Task')"
+      />
+      <record-browser
+        :archetype="archetypeNamed('Task')"
+      />
+    </template>
+
+    <template v-else-if="activeMenuTab === 'events'">
+      <record-browser
+        :archetype="archetypeNamed('Event')"
+      />
+    </template>
+
+    <template v-else-if="activeMenuTab === 'goals'">
+      <record-browser
+        :archetype="archetypeNamed('Goal')"
+      />
+    </template>
     
-    <template v-if="$store.state.activeMenuTab === 'types'">
+    <template v-else-if="activeMenuTab === 'archetypes'">
       <archetype-browser/>
     </template>
     
-    <template v-if="$store.state.activeMenuTab === 'settings'">
+    <template v-else-if="activeMenuTab === 'settings'">
       <flex-menu-tile label="Database" icon="database" />
     </template>
   
@@ -25,12 +68,22 @@
 import RecordBrowser from '@/components/RecordBrowser.vue'
 import ArchetypeBrowser from '@/components/ArchetypeBrowser.vue'
 import FlexMenuTile from '@/components/FlexMenuTile.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     RecordBrowser,
     ArchetypeBrowser,
     FlexMenuTile
+  },
+  computed: {
+    ...mapState(['activeMenuTab']),
+    sortedArchetypes() {
+      return this.$store.getters.archetypes.sort((a, b) => a.name > b.name ? 1 : -1)
+    },
+    archetypeNamed() {
+      return name => this.$store.getters.archetypes.find(arch => arch.name === name)
+    }
   }
 }
 </script>
